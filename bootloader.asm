451ec1deaabb0c1b1fc6624314a0ecdd95d00219
mars-86 <hrslash86@gmail.com>
2021-09-27 14:12:00 -0400

    change loading address for stage 2

--- bootloader.asm
+++ bootloader.asm
@@ -1,22 +1,8 @@
 [bits 16]
-[org 0x7c00]      					; Bootloader starts at physical address 0x7c00
+[org 0x7c00]      							; Bootloader starts at physical address 0x7c00
 
 jmp start
 
-;*******************************************************
-;	Preprocessor directives
-;*******************************************************
-
-;*******************************************************
-;	Data section
-;*******************************************************
-
-message_floppy_reset db 'Reseting Floppy...', 0x0A, 0x0D, 0
-message_read_into_mem db 'Loading to memory...', 0x0A, 0x0D, 0
-message_read_into_mem_success db 'Successfully loaded.', 0x0A, 0x0D, 0
-message_read_into_mem_error db 'Error', 0x0A, 0x0D, 0
-message_halt_due_to_error db 'Halting system due to errors', 0x0A, 0x0D, 0
-read_into_mem_retries dw 3
 
 print_string:
 	push ax
@@ -34,46 +20,42 @@ print_string:
 	pop ax
 	ret
 
-											; At start bootloader sets DL to boot drive
+message_floppy_reset db 'Reseting Floppy...', 0x0A, 0x0D, 0
+message_read_into_mem db 'Loading to memory...', 0x0A, 0x0D, 0
+message_read_into_mem_success db 'Successfully loaded.', 0x0A, 0x0D, 0
+message_read_into_mem_error db 'Error', 0x0A, 0x0D, 0
+message_halt_due_to_error db 'Halting system due to errors', 0x0A, 0x0D, 0
+read_into_mem_retries dw 3
 
-											; Since we specified an ORG(offset) of 0x7c00 we should make sure that
-											; Data Segment (DS) is set accordingly. The DS:Offset that would work
-											; in this case is DS=0 . That would map to segment:offset 0x0000:0x7c00
-											; which is physical memory address (0x0000<<4)+0x7c00 . We can't rely on
-											; DS being set to what we expect upon jumping to our code so we set it
-											; explicitly
-    
+
 start:
 
-xor ax, ax
-mov ds, ax        							; DS=0
+xor ax, ax									; Set DS manually because we specified an ORG
+mov ds, ax        							; DS = 0
 
-cli               							; Turn off interrupts for SS:SP update
-											; to avoid a problem with buggy 8088 CPUs
+cli               							
+
 mov ss, ax        							; SS = 0x0000
-mov sp, 0x7c00    							; SP = 0x7c00
-											; We'll set the stack starting just below
-											; where the bootloader is at 0x0:0x7c00. The
-											; stack can be placed anywhere in usable and
-											; unused RAM.
-sti              							; Turn interrupts back on
+mov sp, 0x77FF    							; SP = 0x77FF
+
+sti              							
 
 mov si, message_floppy_reset
 call print_string
 
-reset_floppy:                				; Resets floppy drive
+reset_floppy:                				; Resets floppy
     xor ax, ax         						; AH = 0 = Reset floppy disk
     int 0x13
-    jc reset_floppy         				; If carry flag was set, try again
+    jc reset_floppy         				; CF set means error, try again
 
 mov si, message_read_into_mem
 call print_string
 
-mov ax, 0x07e0     							; When we read the sector, we are going to read to
-											;    address 0x07e0:0x0000 (phys address 0x07e00)
-											;    right after the bootloader in memory
-mov es, ax         							; Set ES with 0x07e0
-xor bx, bx         							; Offset to read sector to
+mov ax, 0x0050     							; When we read the sector, we are going to read to
+											; address 0x0050:0x7300 (phys address 0x07800)
+											; 1Kb right before the bootloader in memory
+mov es, ax         							; Set ES with 0x0050
+mov bx, 0x7300    							; Offset to read sector to 0x7300
 
 mov si, message_read_into_mem_error
 
@@ -101,16 +83,16 @@ read_to_mem:
 mov si, message_read_into_mem_success
 call print_string
 
-jmp 0x07e0:0x0000 							; Jump to 0x7e0:0x0000 setting CS to 0x07e0
-											; IP to 0 which is code in second stage
-											; (0x07e0<<4)+0x0000 = 0x07e00 physical address
+jmp 0x0050:0x7300 							; Jump to 0x50:0x7300 setting CS to 0x0050
+											; IP to 7300 which is code in second stage
+											; (0x0050 << 4) + 0x7300 = 0x7800 physical address
 
 .error:
 
 mov si, message_halt_due_to_error
 call print_string
 
-jmp $										; endless loop
+jmp $										; Endless loop
 
 times 510 - ($ - $$) db 0   				; Fill the rest of sector with 0
-dw 0xAA55                   				; This is the boot signature
+dw 0xAA55                   				; Boot signature
