ee2192a8d2d86a240b25402788000440f6cd42a4
mars-86 <hrslash86@gmail.com>
2021-09-29 17:15:25 -0400

    files reorganization

--- x16/x16.asm
+++ x16/x16.asm
@@ -3,7 +3,7 @@
 
 jmp x_16
 
-%include "x16/stdio.inc"
+%include "x16/lib/stdio.inc"
 
 welcome_x16 db "Welcome to x16 mode!", 0xA, 0xD, 0
 welcome_x16_len equ $ - welcome_x16
@@ -21,63 +21,9 @@ mov al, 0x13
 int 0x10
 pop es
 
+mov cx, 0x00
 mov al, 'H'
 call putc_16
-
-mov al, 'e'
-call putc_16
-
-mov al, 'l'
-call putc_16
-
-mov al, 'l'
-call putc_16
-
-mov al, 'o'
-call putc_16
-
-mov al, ' '
-call putc_16
-
-mov al, 'w'
-call putc_16
-
-mov al, 'o'
-call putc_16
-
-mov al, 'r'
-call putc_16
-
-mov al, 'l'
-call putc_16
-
-mov al, 'd'
-call putc_16
-
-mov al, ' '
-call putc_16
-
-mov al, 'x'
-call putc_16
-
-mov al, '1'
-call putc_16
-
-mov al, '6'
-call putc_16
-
-mov al, '!'
-call putc_16
-
-; push welcome_x16
-; push welcome_x16_len
-; call print_string_16
-; add sp, 4
-
-; push welcome_x16
-; push welcome_x16_len
-; call print_string_16
-; add sp, 4
 
 mov ah, 0x0C
 mov al, 0xA5
