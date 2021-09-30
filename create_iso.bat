1128b76ae2abc5c24636cd205cc6cb9e96db1e9f
mars-86 <hrslash86@gmail.com>
2021-09-30 19:39:54 -0300

    refac batch file

--- create_iso.bat
+++ create_iso.bat
@@ -1,13 +1,16 @@
 @echo off
 nasm -f bin -o bootloader.bin bootloader.asm
 nasm -f bin -o stage2.bin stage2.asm
+nasm -f bin -o x16.bin x16/x16.asm
 rmdir /q /s ../cdiso
 md ../cdiso
-mv bootloader.bin stage2.bin ../cdiso
+mv bootloader.bin stage2.bin x16.bin ../cdiso
 cd ../cdiso
 dd if=/dev/zero of=disk.img bs=1024 count=720
 dd if=bootloader.bin of=disk.img conv=notrunc
 dd if=stage2.bin of=disk.img bs=512 seek=1 conv=notrunc
-rm bootloader.bin stage2.bin
+dd if=x16.bin of=disk.img bs=512 seek=3 conv=notrunc
+rm bootloader.bin stage2.bin x16.bin
 echo SUCCESS
 exit
+
